b541cbfd4c0faefa660b52010cc6b63214e48644
sergey.efimov <sefimov44@gmail.com>
2019-12-18 16:26:13 +0300

    Fixed properties path for CI

--- src/main/java/com/btto/core/CoreApplication.java
+++ src/main/java/com/btto/core/CoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("btto.properties")
+@PropertySource("classpath:btto.properties")
 public class CoreApplication {
 
     public static void main(String[] args) {
